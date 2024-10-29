90f7b72370da592994281c4271e6ff970ee2d52a
ICarlota99 <k431999@gmail.com>
2024-10-29 15:33:46 -0400

    start setting up header & footer, cookies and server

--- todo_list_v0.2/index.js
+++ todo_list_v0.2/index.js
@@ -1,19 +1,35 @@
 // Import npm modules
 import express from "express";
 import bodyParser from "body-parser";
+import ejs from "ejs";
+import cookieParser from "cookie-parser";
 
 // Declarations
 const app = express();
 const port = 3000;
 
 // Setup npm modules
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(cookieParser());
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}.`);
 });
 
-app.get("/", (req,res)=> {
-    res.render("index.ejs");
-});
+app.get('/', (req, res) => {
+    // Set the 'firstVisit' cookie if it doesn't exist yet
+    if (!req.cookies.firstVisit) {
+        res.cookie('firstVisit', true, { maxAge: 900000, httpOnly: true }); // Set cookie for 15 minutes
+      } 
+  
+      // Now, the 'firstVisit' cookie is guaranteed to exist
+      if (!req.cookies.firstVisit) {
+        // Render the welcome page
+        res.render('welcome.ejs');
+      } else {
+        // Render the regular homepage
+        res.render('index.ejs');
+      }
+    });
