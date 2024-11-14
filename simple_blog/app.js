1b65e1853242f243c333b7073bc446a13ba75949
ICarlota99 <k431999@gmail.com>
2024-11-14 18:05:38 -0500

    add new entries and read each one in a new page

--- simple_blog/app.js
+++ simple_blog/app.js
@@ -22,22 +22,41 @@ const homeStartingContent = "Lorem ipsum dolor sit amet. Nam recusandae sunt qui
 const aboutContent = "Lorem ipsum dolor sit amet. Aut assumenda necessitatibus sed veritatis error et autem doloribus. Nam modi quia sed rerum rerum in accusamus veniam eos repellendus nisi ut rerum praesentium et voluptatibus aliquid. Qui voluptatem maxime ab explicabo ipsam ea ratione magnam cum temporibus natus. Eos beatae quia non illum enim non ipsa sequi et asperiores autem aut reprehenderit iste. Et reiciendis voluptates et facere culpa ut galisum modi ad labore dolores vel autem sequi 33 reiciendis nobis non voluptates aspernatur? Aut magni amet sit ratione eius aut placeat nobis et sequi blanditiis aut suscipit laboriosam. Aut voluptate sint aut repellat veritatis ut provident quasi sed eaque placeat qui voluptatem sint in tempora sunt qui aperiam amet. In dignissimos ipsa aut nemo adipisci et dolorem perspiciatis non repellat saepe et eaque dolorem eum repudiandae omnis. 33 illo quia sit nesciunt quam et consequatur perferendis aut temporibus officia?"
 const contactContent = "Et reiciendis voluptates et facere culpa ut galisum modi ad labore dolores vel autem sequi 33 reiciendis nobis non voluptates aspernatur? Aut magni amet sit ratione eius aut placeat nobis et sequi blanditiis aut suscipit laboriosam."
 
+// Globals
+let posts = [];
+
 //Routes
 app.get('/', (req, res)=>{
-    res.render("index.ejs", {header: homeHeader, content: homeStartingContent});
+    res.render("index.ejs", {
+        header: homeHeader, 
+        content: homeStartingContent,
+        posts: posts,
+    });
 });
 app.get('/about', (req, res)=>{
-    res.render("index.ejs", {header: aboutHeader, content: aboutContent});
+    res.render("about.ejs", {header: aboutHeader, content: aboutContent});
 });
 app.get('/contact', (req, res)=>{
-    res.render("index.ejs", {header: contactHeader, content: contactContent});
+    res.render("contact.ejs", {header: contactHeader, content: contactContent});
 });
 app.get('/compose', (req, res)=>{
     res.render("compose.ejs", {header: composeHeader});
 });
-app.post('/newEntry', (req, res)=>{
-    let newEntry = req.body.newEntry;
-    console.log(newEntry);
+app.get('/posts/:post', (req, res)=>{
+    res.render("posts.ejs", {
+        header: posts[req.params.post].title,
+        content: posts[req.params.post].content,
+    });
+});
+app.post('/newPost', (req, res)=>{
+    
+    const post = {
+        title: req.body.entryTitle,
+        content: req.body.entryContent
+    };
+    posts.push(post);
+
+    res.redirect('/');
 });
 
 
