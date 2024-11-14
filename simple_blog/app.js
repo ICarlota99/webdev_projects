f544d7f75e61a30b73ef91a2da4c73c7bc74147a
ICarlota99 <k431999@gmail.com>
2024-11-14 12:25:39 -0500

    setup initial configuration

---
+++ simple_blog/app.js
@@ -0,0 +1,19 @@
+// Import npm modules
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+
+// Declarations
+const app = express();
+const port = 3000;
+
+// Setup npm modules
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+
+
+app.listen(port, ()=>{
+    console.log(`App listening on port ${port}.`);
+})
